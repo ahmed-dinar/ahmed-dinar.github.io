39e2f4fbebfb4deed65b029d0a5d3e00a74204ce
ahmed-dinar <madinar.cse@gmail.com>
2017-10-23 23:49:42 +0600

    add twitter

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,64 +1,57 @@
-$(document).ready(function(){
+    var myselfs = [
+        'PROBLEM SOLVER',
+        'COFFEE LOVER',
+        'TECHNOLOGY ENTHUSIAST',
+        'COMPETITIVE PROGRAMMING ENTHUSIAST',
+        'FULL STACK DEVELOPER'
+    ];
+    var my = 0;
 
-    /** 
-     * This part does the "fixed navigation after scroll" functionality
-     * We use the jQuery function scroll() to recalculate our variables as the 
-     * page is scrolled/
-     */
-    $(window).scroll(function(){
-        var window_top = $(window).scrollTop() + 12; // the "12" should equal the margin-top value for nav.stick
-        var div_top = $('#nav-anchor').offset().top;
-            if (window_top > div_top) {
-                $('nav').addClass('stick');
-            } else {
-                $('nav').removeClass('stick');
-            }
+    $(document).ready(function() {
+
+        $('.tips').tipsy({
+            fade: false,
+            gravity: 'n'
+        });
+
+      $('.myself').text(myselfs[my].toUpperCase());
+      setInterval(quoteTimer, 3000);
+
+
+      $(".nav-link").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+          // Prevent default anchor click behavior
+          event.preventDefault();
+
+          // Store hash
+          var hash = this.hash;
+
+          // Using jQuery's animate() method to add smooth page scroll
+          // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top
+          }, 800, function(){
+       
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+          });
+        } // End if
+      });
+
+
     });
 
-    /**
-     * This part causes smooth scrolling using scrollto.js
-     * We target all a tags inside the nav, and apply the scrollto.js to it.
-     */
-    $("nav a").click(function(evn){
-        evn.preventDefault();
-        $('html,body').scrollTo(this.hash, this.hash); 
-    });
+    var quoteTimer = function() {
 
-    /**
-     * This part handles the highlighting functionality.
-     * We use the scroll functionality again, some array creation and 
-     * manipulation, class adding and class removing, and conditional testing
-     */
-    var aChildren = $("nav li").children(); // find the a children of the list items
-    var aArray = []; // create the empty aArray
-    for (var i=0; i < aChildren.length; i++) {    
-        var aChild = aChildren[i];
-        var ahref = $(aChild).attr('href');
-        aArray.push(ahref);
-    } // this for loop fills the aArray with attribute href values
-
-    $(window).scroll(function(){
-        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
-        var windowHeight = $(window).height(); // get the height of the window
-        var docHeight = $(document).height();
-
-        for (var i=0; i < aArray.length; i++) {
-            var theID = aArray[i];
-            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
-            var divHeight = $(theID).height(); // get the height of the div in question
-            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
-                $("a[href='" + theID + "']").addClass("nav-active");
-            } else {
-                $("a[href='" + theID + "']").removeClass("nav-active");
-            }
-        }
-
-        if(windowPos + windowHeight == docHeight) {
-            if (!$("nav li:last-child a").hasClass("nav-active")) {
-                var navActiveCurrent = $(".nav-active").attr("href");
-                $("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
-                $("nav li:last-child a").addClass("nav-active");
-            }
-        }
-    });
-});
+      if (++my >= myselfs.length)
+        my = 0;
+      
+      $('.myself').fadeOut(200, function(){
+        // $(this).text(myselfs[my].toUpperCase());
+        $(this).text(myselfs[my].toUpperCase());
+        $(this).fadeIn();
+      });
+      
+    }
