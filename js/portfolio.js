10b9fc32a87ede85c8471f7e6e93829d0121f138
ahmed-dinar <madinar.cse@gmail.com>
2017-10-28 02:27:51 +0600

    js fix

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,19 +1,6 @@
 
 
 
-    // ===== Scroll to Top (c) https://codepen.io/rdallaire/pen/apoyx/ ==== 
-    $(window).scroll(function() {
-        if ($(this).scrollTop() >= 100) {        // If page is scrolled more than 50px
-            $('#return-to-top').fadeIn(400);    // Fade in the arrow
-        } else {
-            $('#return-to-top').fadeOut(400);   // Else fade out the arrow
-        }
-    });
-    $('#return-to-top').click(function() {      // When arrow is clicked
-        $('body,html').animate({
-            scrollTop : 0                       // Scroll to top of body
-        }, 1000);
-    });
 
 
 
@@ -70,7 +57,26 @@
       });
 
 
+
+
+      // ===== Scroll to Top (c) https://codepen.io/rdallaire/pen/apoyx/ ==== 
+      $(window).scroll(function() {
+          if ($(this).scrollTop() >= 100) {        // If page is scrolled more than 50px
+              $('#return-to-top').fadeIn(400);    // Fade in the arrow
+          } else {
+              $('#return-to-top').fadeOut(400);   // Else fade out the arrow
+          }
+      });
+      
+      $('#return-to-top').click(function() {      // When arrow is clicked
+          $('body,html').animate({
+              scrollTop : 0                       // Scroll to top of body
+          }, 1000);
+      });
+
     });
+
+
 
     var quoteTimer = function() {
 
